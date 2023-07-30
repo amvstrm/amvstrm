cd8d41c8ca29e60b50674a4373d1a82a542a130a
nyt92 <chamroeunlinda12343@gmail.com>
2023-07-30 14:24:45 +0700

    update about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,55 +1,63 @@
 <script setup>
 useSeoMeta({
-  ogTitle: 'About',
-  ogDescription: 'About amvstrm...',
-  ogImage: 'logo.png',
-  ogUrl: '[og:url]',
-  twitterTitle: 'About',
-  twitterDescription: 'About amvstrm...',
-  twitterImage: 'logo.png',
-  twitterCard: 'summary'
-})
+  ogTitle: "About",
+  ogDescription: "About amvstrm...",
+  ogImage: "logo.png",
+  ogUrl: "[og:url]",
+  twitterTitle: "About",
+  twitterDescription: "About amvstrm...",
+  twitterImage: "logo.png",
+  twitterCard: "summary",
+});
 
 useHead({
   htmlAttrs: {
-    lang: 'en'
+    lang: "en",
   },
-  title: "About"
-})
-
+  title: "About",
+});
 </script>
 <template>
   <v-container>
-    <v-card>
-      <v-card-title>About amvstrm</v-card-title>
-      <v-card-text>
-        amvstrm is an open source anime streaming platform that offer free anime
-        with no ADs and install it as Android or IOS web app. <br>
-        Build with Nuxt 3, Vuetify and amvstrm's V2 API.
-      </v-card-text>
-    </v-card>
-    <v-row class="my-5">
-      <v-col cols="12">
-        <h1>Other :</h1>
-      </v-col>
-      <v-col cols="12">
-        <v-btn
-          prepend-icon="mdi-github"
-          href="https://github.com/amvstrm/amvstrm"
-        >
-          Github
-        </v-btn>
-      </v-col>
-      <v-col cols="12">
-        <v-btn prepend-icon="mdi-github" href="https://github.com/amvstrm/api">
-          Github (API)
-        </v-btn>
-      </v-col>
-      <v-col cols="12">
-        <v-btn prepend-icon="mdi-bookshelf" href="https://docs.amvstr.ml">
-          Docs
-        </v-btn>
-      </v-col>
-    </v-row>
+    <div style="height: fit-content; display: grid; place-items: center">
+      <div class="pa-5" style="background-color: #212121">
+        <h1><img src="/logo.png" alt="loho" height="50" /></h1>
+        <p>
+          amvstrm is an open source anime streaming platform that offer free
+          anime with no ADs and install it as Android or IOS web app. <br />
+          Build with Nuxt 3, Vuetify and amvstrm's V2 API.
+        </p>
+        <div class="mt-2">
+          <v-btn
+            class="mr-2"
+            color="grey-darken-1"
+            prepend-icon="mdi-github"
+            href="https://github.com/amvstrm"
+          >
+            Github
+          </v-btn>
+          <v-btn
+            color="blue-darken-1"
+            prepend-icon="mdi-file-document"
+            href="https://amvdocs.pages.dev"
+          >
+            Documentation
+          </v-btn>
+        </div>
+        <div class="my-5">
+          <h1>Donation</h1>
+          We do not use ADs and rely on donation to keep the website running forever. 
+          You can consider donating us to keep it alive! <br>
+          <v-btn
+            class="mt-4"
+            color="yellow-darken-1"
+            prepend-icon="mdi-cash"
+            href="https://en.liberapay.com/amvstrm"
+          >
+            Donation
+          </v-btn>
+        </div>
+      </div>
+    </div>
   </v-container>
 </template>
