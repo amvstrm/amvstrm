3f54f0421218922ab18f53116938196b69521cff
nyt92 <chamroeunlinda12343@gmail.com>
2023-08-23 14:12:25 +0700

    removed logo from seo meta

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,9 @@
 useSeoMeta({
   ogTitle: "Home",
   ogDescription: "amvstrm - A streaming service for weebo...",
-  ogImage: "logo.png",
   ogUrl: useRoute().fullPath,
   twitterTitle: "Home",
   twitterDescription: "amvstrm - A streaming service for weebo...",
-  twitterImage: "logo.png",
   twitterCard: "summary",
 });
 
