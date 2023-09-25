866156250c8b55e354ef099cdea0116603abe439
nyt92 <chamroeunlinda12343@gmail.com>
2023-09-25 18:59:17 +0700

    added web mode

--- pages/pwa/more.vue
+++ pages/pwa/more.vue
@@ -12,12 +12,12 @@
     <div class="pb-4">
       <h1>More</h1>
     </div>
-    <v-list lines="three">
+    <v-list lines="two">
       <v-list-item title="amvstrm's PWA mode" subtitle="Version amvN3 (Nuxt 3)">
         <template #prepend>
           <img
             src="/icon.png"
-            style="display: flex; width: 140px; height: 100%"
+            style="display: flex; width: 100px; height: 100%"
           />
         </template>
       </v-list-item>
@@ -28,6 +28,7 @@
     </v-list>
     <v-divider class="my-4"></v-divider>
     <v-list lines="two">
+      <v-list-item title="Web Mode" append-icon="mdi-web" to="/"></v-list-item>
       <v-list-item title="About us" target="blank" href="/about" />
       <v-list-item title="Privacy Policy" target="blank" href="/privacy" />
       <v-list-item
