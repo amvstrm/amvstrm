ba80db7f2ffc93955723307677824efc7815cff0
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:27:45 +0700

    new comment block component

---
+++ components/WComps/CommentBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        <v-icon class="mr-2" icon="mdi-chat" />
+        Comments
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <div class="my-2">
+          <DisqusComments :identifier="/\/pwa\.*/.test(useRoute().path) ? useRoute().fullPath.replace(/\/pwa\//, '') : useRoute().fullPath" />
+        </div>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
