041aa4522e75494fa03595acc04f200771f19c2f
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:30:32 +0700

    new breadcrumb bar

--- layouts/webapp.vue
+++ layouts/webapp.vue
@@ -1,5 +1,21 @@
 <template>
   <v-app>
+    <v-app-bar absolute="">
+      <v-breadcrumbs :items="breadcrumbs">
+        <template #prepend>
+          <v-icon class="mr-1 d-flex" size="small" icon="mdi-home"></v-icon>
+        </template>
+        <template #item="{ item }">
+          <v-breadcrumbs-item
+            :class="shouldDisableLink(item.link) ? '' : 'truncated'"
+            :to="item.link"
+            :disabled="shouldDisableLink(item.link)"
+          >
+            {{ item.text }}
+          </v-breadcrumbs-item>
+        </template>
+      </v-breadcrumbs>
+    </v-app-bar>
     <v-main>
       <slot />
     </v-main>
@@ -23,3 +39,52 @@
     </v-bottom-navigation>
   </v-app>
 </template>
+<script setup>
+const route = useRoute();
+
+function generateBreadcrumbs(route) {
+  const breadcrumbs = [];
+  breadcrumbs.push({ text: "Home", link: "/pwa" });
+
+  const pathSegments = route.path.slice(4).split("/");
+  for (let i = 1; i < pathSegments.length; i++) {
+    const segment = pathSegments[i];
+    const text = segment.charAt(0).toUpperCase() + segment.slice(1);
+    const link = `${pathSegments.slice(0, i + 1).join("/")}`;
+    breadcrumbs.push({ text, link });
+  }
+
+  return breadcrumbs;
+}
+
+function shouldDisableLink(link) {
+  const nonClickablePaths = [
+    "/watch",
+    "/anime",
+    "/more",
+    "/search",
+    "/bookmarks",
+  ];
+  return nonClickablePaths.some((path) => link.startsWith(path));
+}
+
+const breadcrumbs = computed(() => generateBreadcrumbs(route));
+</script>
+<style>
+.truncated {
+  width: "180px";
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+@media (min-width: 768px) {
+  .truncated {
+    width: "250px";
+  }
+}
+
+.v-breadcrumbs-item--disabled {
+  opacity: 1 !important;
+}
+</style>
