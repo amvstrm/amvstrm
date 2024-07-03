cd4990295529ed77a1edb596a828d49ad5d98468
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:33:30 +0700

    remove breadcrumb

--- pages/pwa/more.vue
+++ pages/pwa/more.vue
@@ -1,14 +1,6 @@
 <!-- eslint-disable vue/no-multiple-template-root -->
 <template>
   <v-container>
-    <v-breadcrumbs style="padding: 0; margin-bottom: 1rem">
-      <template #prepend>
-        <v-icon size="small" icon="mdi-home"></v-icon>
-      </template>
-      <v-breadcrumbs-item title="Home" to="/pwa" />
-      <v-breadcrumbs-divider />
-      <v-breadcrumbs-item title="More" />
-    </v-breadcrumbs>
     <div class="pb-4">
       <h1>More</h1>
     </div>
@@ -52,3 +44,11 @@
     </v-list>
   </v-container>
 </template>
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  title: "More",
+});
+</script>
