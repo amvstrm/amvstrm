37c03ea2c73bf27080f1d4c5ba5797429d105b54
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:33:18 +0700

    added raw url download button

--- pages/download/[id].vue
+++ pages/download/[id].vue
@@ -5,6 +5,8 @@ const getID = useRoute().params.id.split("-")[0];
 const getGogoID = useRoute().params.id.split(`${getID}-`)[1];
 const getEP = getGogoID.split(`-episode-`)[1];
 
+const dl_dialog = ref(false);
+
 const { data: anime } = await useFetch(
   `${env.public.API_URL}/api/${env.public.version}/info/${getID}`,
   {
@@ -19,6 +21,14 @@ const { data: animedl, pending: aniDLpending } = useFetch(
   }
 );
 
+const { data: dlURL } = useFetch(
+  `https://api3.iotserver24.workers.dev/download/${getGogoID}`,
+  {
+    cache: "force-cache",
+    responseType: "json",
+  }
+);
+
 useHead({
   htmlAttrs: {
     lang: "en",
@@ -30,7 +40,54 @@ useHead({
 </script>
 
 <template>
-  <div class="app-page">
+  <v-container>
+    <v-dialog v-model="dl_dialog" max-width="500px" scrim="#191919">
+      <v-card>
+        <v-card-title>Download</v-card-title>
+        <v-card-text class="d-flex flex-column ga-2">
+          <v-btn
+            prepend-icon="mdi-download"
+            :href="dlURL.results['640x360']"
+            color="red"
+            :disabled="dlURL.results['640x360'] === ''"
+          >
+            360p
+          </v-btn>
+          <v-btn
+            prepend-icon="mdi-download"
+            :href="dlURL.results['854x480']"
+            color="red"
+            :disabled="dlURL.results['854x480'] === ''"
+          >
+            480p
+          </v-btn>
+          <v-btn
+            prepend-icon="mdi-download"
+            :href="dlURL.results['1280x720']"
+            color="red"
+            :disabled="dlURL.results['1280x720'] === ''"
+          >
+            720p (HD)
+          </v-btn>
+          <v-btn
+            prepend-icon="mdi-download"
+            :href="dlURL.results['1920x1080']"
+            color="red"
+            :disabled="dlURL.results['1920x1080'] === ''"
+          >
+            1080p (FHD)
+          </v-btn>
+
+          <i class="d-block text-gray">
+            API is provided by
+            <a href="https://api3.iotserver24.workers.dev/">iotserver24</a>
+          </i>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn prepend-icon="mdi-close" @click="dl_dialog = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-sheet
       elevation="12"
       max-width="600"
@@ -40,11 +97,25 @@ useHead({
     >
       <v-icon class="mb-5" color="blue" icon="mdi-download" size="112" />
       <h2 class="mb-6">Download</h2>
-      <p class="mb-4">
+      <h3 class="mb-2">
         {{ anime?.title.userPreferred + " Episode " + getEP }}
-      </p>
+        <br />
+      </h3>
+      <em class="pb-4 d-block" style="color: red">
+        *This will redirected to Anitaku download page!
+      </em>
       <v-divider class="mb-4" />
-      <div class="text-end">
+      <div class="text-end ga-2 d-flex flex-row-reverse">
+        <v-btn
+          :to="'/watch/' + useRoute().params.id"
+          prepend-icon="mdi-play"
+          color="red"
+        >
+          Watch
+        </v-btn>
+        <v-btn color="yellow" prepend-icon="mdi-link" @click="dl_dialog = true">
+          Raw
+        </v-btn>
         <v-btn
           :loading="aniDLpending"
           :href="animedl?.download"
@@ -56,12 +127,5 @@ useHead({
         </v-btn>
       </div>
     </v-sheet>
-  </div>
+  </v-container>
 </template>
-<style>
-.app-page {
-  height: 90vh;
-  display: grid;
-  place-items: center;
-}
-</style>
