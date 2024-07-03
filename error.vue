96ec747d490a05955e036aa96ebfa08ef4a46f0e
nyt92 <chamroeunlinda12343@gmail.com>
2024-07-03 21:22:40 +0700

    new error page

--- error.vue
+++ error.vue
@@ -6,43 +6,21 @@
       </Title>
     </Head>
   </Html>
-  <div class="app-error-page">
-    <v-sheet
-      elevation="12"
-      max-width="600"
-      rounded="lg"
-      width="100%"
-      class="ma-2 pa-4 text-center mx-auto"
-    >
-      <v-icon class="mb-5" color="error" icon="mdi-exclamation" size="112" />
-      <h2 class="mb-6">
-        Error
-      </h2>
-      <p class="mb-4">
-        {{ error.statusCode === 404 ? 'Page not found!' : 'Internal server error!' }}
-      </p>
-      <v-divider class="mb-4" />
-      <div class="text-end">
-        <v-btn class="mr-1" :href="/\/pwa\.*/.test(useRoute().path) ? '/pwa' : '/'" prepend-icon="mdi-home"> Go home </v-btn>
-        <v-btn
-          v-if="error.statusCode !== 404"
-          href="https://github.com/amvstrm/amvstrm/issues/new"
-          target="blank"
-          color="primary"
-          prepend-icon="mdi-pencil"
-        >
-          Report Issue
-        </v-btn>
-      </div>
-    </v-sheet>
-  </div>
+  <NuxtLayout :name="/\/pwa\.*/.test(useRoute().path) ? 'webapp' : 'default'">
+    <v-empty-state
+      :headline="error.statusCode || '500'"
+      title="Error"
+      :text="error.message || 'Something went wrong'"
+      image="/logo.png"
+    ></v-empty-state>
+  </NuxtLayout>
 </template>
 <script setup>
 defineProps({
   // eslint-disable-next-line vue/require-default-prop
   error: Object,
 });
-console.log(error)
+console.log(error);
 </script>
 <style>
 .app-error-page {
